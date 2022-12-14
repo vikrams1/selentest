4600bc053b0b0f69d5925070bb89a41fa4aff6c1
vikram <vik_brlee@yahoo.com>
2022-12-14 12:46:31 +0530

    first

--- src/test/java/selenium/GitHubLogin.java
+++ src/test/java/selenium/GitHubLogin.java
@@ -23,10 +23,7 @@ public class GitHubLogin {
 	// Logout - @AC
 	// Quit - @AS
 
-	//public WebDriver driver;
-	
-    ChromeOptions chromeOptions = new ChromeOptions();
-    WebDriver driver = new ChromeDriver(chromeOptions);
+	public WebDriver driver;
 	
 
 	@BeforeSuite
@@ -36,11 +33,11 @@ public class GitHubLogin {
 		//driver = new ChromeDriver();
 		
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-      //  ChromeOptions chromeOptions = new ChromeOptions();
+        //ChromeOptions chromeOptions = new ChromeOptions();
         //chromeOptions.addArguments("--headless");
         //chromeOptions.addArguments("--no-sandbox");
         
-         driver = new ChromeDriver(chromeOptions);
+         driver = new ChromeDriver();
 		
 		//ExtentSparkReporter htmlReporter = new ExtentSparkReporter("extentReport.html");
 	}
